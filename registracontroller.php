158ad3479221240a3b7f1d9f165133fb4c336270
silviaarnoldi <101811166+silviaarnoldi@users.noreply.github.com>
2023-12-19 17:05:00 +0000

    www

--- registracontroller.php
+++ registracontroller.php
@@ -6,20 +6,28 @@ $username=$_POST['username'];
 $password=$_POST['password'];
 
 $password=MD5($password);
-echo("ok");
 
 $connesione= new mysqli('localhost','root','','MaintHelp'); 
 if($connesione->connect_error){
     echo("Connection failed: " . $connesione->connect_error);
     exit();
 }else{
-    try{
-        $registra="insert into UTENTE (nome, cognome, ruolo, username, password) values ('$nome', '$cognome', '$ruolo', '$username', '$password')";
-        $connesione->query($registra);
-        header("Location: homeAmministratore.php");
-    }catch(Exception $e){
-        $err = $e->getMessage();
-        header("Location: registra.php?err=$err");
+    // Controllo se il nome utente esiste già
+    $checkUsername = "SELECT * FROM utenti WHERE username='$username'";
+    $result = $connesione->query($checkUsername);
+
+    if ($result->num_rows > 0) {
+        echo "Il nome utente esiste già. Si prega di scegliere un nome utente diverso.";
+        header("Location: registra.php");
+    } else {
+        try{
+            $registra="insert into UTENTE (nome, cognome, ruolo, username, password) values ('$nome', '$cognome', '$ruolo', '$username', '$password')";
+            $connesione->query($registra);
+            header("Location: homeAmministratore.php");
+        }catch(Exception $e){
+            $err = $e->getMessage();
+            header("Location: registra.php?err=$err");
+        }
     }
 }
 ?>
