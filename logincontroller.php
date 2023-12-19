90418b1fd315922234475b19b3d3347d69c3981b
silviaarnoldi <101811166+silviaarnoldi@users.noreply.github.com>
2023-12-19 08:20:37 +0000

    progetto2

--- logincontroller.php
+++ logincontroller.php
@@ -14,7 +14,7 @@ if($connesione->connect_error){
     try{
         $verifica="select * from UTENTE where USERNAME='$username' and PASSWORD='$password';";
         $result=$connesione->query($verifica);
-        echo("risultat".$result->num_rows>0);
+        echo("risultat".$result->num_rows);
         if($result->num_rows>0){
             while($user=$result->fetch_array(MYSQLI_ASSOC)){
                 echo("id:".$user['id']);
