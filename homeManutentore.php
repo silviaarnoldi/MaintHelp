8fadfffab1407544aa9d5d79be930766952308d7
silviaarnoldi <101811166+silviaarnoldi@users.noreply.github.com>
2024-01-09 19:04:05 +0000

    elenco

--- homeManutentore.php
+++ homeManutentore.php
@@ -26,11 +26,11 @@ if(!isset($_SESSION['ruolo'])){
                     <th>Chiamata manutenzione</th>
                 </tr>
                 <?php
-                $connesione= new mysqli('localhost','root','','MaintHelp'); 
-                if($connesione->connect_error){
-                    echo("Connection failed: " . $connesione->connect_error);
-                    exit();
-                }else{
+                    $connesione= new mysqli('localhost','root','','MaintHelp'); 
+                    if($connesione->connect_error){
+                        echo("Connection failed: " . $connesione->connect_error);
+                        exit();
+                    }else{
                         $query="select * from MACCHINARIO";
                         $result=$connesione->query($query);
                         while($row=$result->fetch_assoc()){
@@ -38,18 +38,18 @@ if(!isset($_SESSION['ruolo'])){
                             echo "<td>".$row['ID']."</td>";
                             echo "<td>".$row['ULTIMA_MANUTENZIONE']."</td>";
                             if($row['PROSSIMA_MANUTENZIONE'] <= date("Y-m-d")) {
-                                echo "<td><a href='manutenzionePreventiva.php'><button>OGGI</button></a></td>";
+                                echo "<td><a href='manutenzionePreventiva.php?ID=".$row['ID']."'><button>OGGI</button></a></td>";
                             } else {
                                 echo "<td>".$row['PROSSIMA_MANUTENZIONE']."</td>";
                             }
                             if($row['CHIAMATA_MANUTENTORE'] == 1) {
-                                echo "<td><a href='manutenzioneGuasto.php'><button>Chiamata Manutenzione</button></a></td>";
+                                echo "<td><a href='manutenzioneGuasto.php?ID=".$row['ID']."'><button>Chiamata Manutenzione</button></a></td>";
                             } else {
                                 echo "<td>NO</td>";
                             }
                             echo "</tr>";
                         }
-                }
+                    }
                 ?>
             </table>
             <br>
--- manutenzionePreventiva.php
+++ manutenzionePreventiva.php
@@ -1,32 +1,47 @@
+<?php
+$connesione= new mysqli('localhost','root','','MaintHelp'); 
+if($connesione->connect_error){
+    echo("Connection failed: " . $connesione->connect_error);
+    exit();
+}
+$id_macchinario = isset($_GET['ID']) ? mysqli_real_escape_string($connesione, $_GET['ID']) : null;
+$query = "SELECT * FROM MACCHINARIO WHERE ID = $id_macchinario";
+$result = mysqli_query($connesione, $query);
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+} else {
+    echo "Nessun risultato trovato";
+}
+
+// Chiusura connessione
+mysqli_close($connesione);
+?>
 <!DOCTYPE html>
 <html lang="">
+<style>
+        div.description {
+            font-size: 22px;
+        }
+      table {
+            margin-left: auto;
+            margin-right: auto;
+        }
+        form {
+            font-size: 22px; 
+        }
+
+        
+    </style>
 <body>
     <center><h1>Manutenzione Preventiva</h1> </center>
+    </center>
     <table>
+        <center>
+        <tr>
         <td>
             <form action="manutenzionePreventivacontroller.php" method="post">
             <label for="data">Data:</label><br>
             <input type="date" id="data" name="data"><br>
-            <label for="macchinario">Macchinario:</label><br>
-            <select id="macchinario" name="macchinario">
-            <?php
-                $conn = new mysqli('localhost', 'root', '', 'MaintHelp');
-                if ($conn->connect_error) {
-                    die("Connessione fallita: " . $conn->connect_error);
-                }
-                $sql = "SELECT ID FROM MACCHINARIO";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo "<option value='" . $row['ID'] . "'>" . $row['ID'] . "</option>";
-                    }
-                } else {
-                    echo "0 results";
-                }
-                $conn->close();
-            ?>
-            </select><br>
             <label for="descrizione">Descrizione:</label><br>
             <textarea id="descrizione" name="descrizione"></textarea><br>
             <label for="ore">Ore di manutenzione:</label><br>
@@ -36,25 +51,30 @@
             <label for="data_prossima">Data della prossima manutenzione:</label><br>
             <input type="date" id="data_prossima" name="data_prossima"><br>
             <label for="id_manutentore">ID Manutentore:</label><br>
-            <input type="text" id="id_manutentore" name="id_manutentore">
-            <br><br>
+            <input type="text" id="id_manutentore" name="id_manutentore"><br> <br>
             <input type="submit" value="Inserisci">
+            <br><br>
         </form>
         </td>
         <td>
             <div class="description">
 						SCHEMA MECCANICO
 			</div> <br>
-            <img src="img/schemaM.png" alt="SCHEMA MECCANICO" style="width:330px;height:530px;">
-            <br><br><br><br>
-            <div class="description">
-						SCHEMA ELETTRICO
-			</div> <br>
-            <img src="img/schemaE.png" alt="SCHEMA ELETTRICO" style="width:330px;height:530px;">
+            <img src="<?php echo $row['SCHEMA_MEC']; ?>" alt="SCHEMA MECCANICO" style="width:580px;height:340px;">
             
         </td>
-
-    </table>
+    </tr>
+    </center>
+    <tr>
+        <td></td>
+        <td>
+        <div class="description">
+						SCHEMA ELETTRICO
+			</div> <br>
+            <img src="<?php echo $row['SCHEMA_ELECT']; ?>" alt="SCHEMA ELETTRICO" style="width:580px;height:340px;">
+        </td>
+    </tr>
+    </table> </center>
     <?php
         if(isset($_GET['error'])){
             echo $_GET['error'];
